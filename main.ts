d0779053d0190495f68780757b3f722d2d4d57ca
giannola <54441894+gioanila@users.noreply.github.com>
2024-12-12 00:09:05 -0300

    tilemaps and initialization

--- main.ts
+++ main.ts
@@ -1,1 +1,95 @@
-
+namespace ConnectionKind {
+    export const Door3 = ConnectionKind.create()
+    export const Door4 = ConnectionKind.create()
+    export const Door5 = ConnectionKind.create()
+}
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+tiles.loadMap(tiles.createMap(tilemap`level2`))
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+characterAnimations.loopFrames(
+mySprite,
+[img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `,img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f 3 3 5 3 3 5 3 d f f . . 
+    . . . f d f 3 5 5 3 f f d f . . 
+    . . . f d f 3 3 3 3 3 f f . . . 
+    . . . f f 3 5 3 3 5 3 3 f . . . 
+    . . . . f f f f f f f f f . . . 
+    . . . . . . . . . f f . . . . . 
+    `,img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 3 f f . . 
+    . . f d f f 3 5 5 3 f d f . . . 
+    . . . f f 3 3 3 3 3 f d f . . . 
+    . . . f 3 3 5 3 3 5 3 f f . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . . f f . . . . . . . . . 
+    `],
+500,
+characterAnimations.rule(Predicate.Moving)
+)
+game.splash("Welcome to the dungeons... you have 15 minutes to leave before they implode on top of you. Find letters by going through 6 rooms and completing the challenges, then create a code in order to escape. Best of luck!")
+let room1 = tiles.createMap(tilemap`level6`)
+let room2 = tiles.createMap(tilemap`level2`)
+let room3 = tiles.createMap(tilemap`level8`)
+let room4 = tiles.createMap(tilemap`level13`)
+let room5 = tiles.createMap(tilemap`level15`)
+let room6 = tiles.createMap(tilemap`level17`)
+tiles.connectMapById(room1, room2, ConnectionKind.Door1)
+tiles.connectMapById(room2, room3, ConnectionKind.Door2)
+tiles.connectMapById(room3, room4, ConnectionKind.Door3)
+tiles.connectMapById(room3, room4, ConnectionKind.Door4)
+tiles.connectMapById(room5, room6, ConnectionKind.Door5)
