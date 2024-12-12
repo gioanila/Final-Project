ff8e221a5e3673695139f4f74a2e5f8b67d9b9ce
giannola <54441894+gioanila@users.noreply.github.com>
2024-12-12 01:00:55 -0300

    room 1 needs fixing with the score

--- main.ts
+++ main.ts
@@ -3,7 +3,42 @@ namespace ConnectionKind {
     export const Door4 = ConnectionKind.create()
     export const Door5 = ConnectionKind.create()
 }
-let mySprite = sprites.create(img`
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+function _1 (R1: boolean) {
+    info.setScore(0)
+    mySprite.sayText("Room 1... looks like I just need to collect 10 fruits.", 3000, false)
+    for (let index = 0; index < 10; index++) {
+        mySprite2 = sprites.create(img`
+            . . . . . . . 6 . . . . . . . . 
+            . . . . . . 8 6 6 . . . 6 8 . . 
+            . . . e e e 8 8 6 6 . 6 7 8 . . 
+            . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+            . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+            e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+            e e 2 e 2 2 4 2 2 e e e c . . . 
+            e e e e 2 e 2 2 e e e c . . . . 
+            e e e 2 e e c e c c c . . . . . 
+            . c c c c c c c . . . . . . . . 
+            `, SpriteKind.Food)
+        mySprite2.setPosition(randint(20, 200), randint(20, 200))
+    }
+    if (info.score() == 10) {
+        game.splash("You passed this level!!")
+        mySprite.sayText("YAY")
+    }
+}
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . 5 . 5 . . . . . . . 
     . . . . . f 5 5 5 f f . . . . . 
     . . . . f 1 5 2 5 1 6 f . . . . 
@@ -21,7 +56,7 @@ let mySprite = sprites.create(img`
     . . . f f f f f f f f f f . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-tiles.loadMap(tiles.createMap(tilemap`level2`))
+tiles.loadMap(tiles.createMap(tilemap`level6`))
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 characterAnimations.loopFrames(
@@ -82,6 +117,9 @@ mySprite,
 characterAnimations.rule(Predicate.Moving)
 )
 game.splash("Welcome to the dungeons... you have 15 minutes to leave before they implode on top of you. Find letters by going through 6 rooms and completing the challenges, then create a code in order to escape. Best of luck!")
+pauseUntil(() => true)
+info.startCountdown(900)
+_1(true)
 let room1 = tiles.createMap(tilemap`level6`)
 let room2 = tiles.createMap(tilemap`level2`)
 let room3 = tiles.createMap(tilemap`level8`)
